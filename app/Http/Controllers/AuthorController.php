4d75e0d36452ee568e2fd2babf006792a4d71422
muhammadafifabdurrahman <muhammadaefif@gmail.com>
2025-10-16 21:22:49 +0700

    update tugas pertemuan4 laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,16 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
-     public function index() {
-       $authors = Author::all();
+  public function index()
+  {
+    $authors = Author::all();
 
-       return response()->json([
+    if ($authors->isEmpty()) {
+      return response()->json([
         "success" => true,
-        "message" => "Get all resources",
-        "data" => $authors
-       ], 200);
+        "message" => "Resource data not found!"
+      ], 200);
     }
+
+    return response()->json([
+      "success" => true,
+      "message" => "Get all resources",
+      "data" => $authors
+    ]);
+  }
+
+  public function store(Request $request)
+  {
+    //1. validator
+    $validator = Validator::make($request->all(), [
+      'name' => 'required|string|max:100',
+      'photo' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+      'bio' => 'required|string|max:100'
+
+    ]);
+
+
+    //2. cek validator error
+    if ($validator->fails()) {
+      return response()->json([
+        'success' => false,
+        'message' => $validator->errors()
+      ], 422);
+    }
+
+
+    //3. upload image
+    $image = $request->file('photo');
+    $image->store('authors', 'public');
+
+
+    //4. insert data
+    $author = Author::create([
+      'name' => $request->name,
+      'photo' => $image->hashName(),
+      'bio' => $request->bio,
+    ]);
+
+
+    //5. response
+    return response()->json([
+      'success' => true,
+      'message' => 'Resource added successfully',
+      'data' => $author
+    ], 201);
+  }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use function PHPUnit\Framework\returnArgument;
 
 class GenreController extends Controller
@@ -11,10 +12,55 @@ class GenreController extends Controller
     public function index() {
        $genres = Genre::all();
 
+       if ($genres->isEmpty()) {
+      return response()->json([
+        "success" => true,
+        "message" => "Resource data not found!"
+      ], 200);
+    }
+
        return response()->json([
         "success" => true,
         "message" => "Get all resources",
         "data" => $genres
-       ], 200);
+       ]);
     }
+
+    public function store(Request $request)
+  {
+    //1. validator
+    $validator = Validator::make($request->all(), [
+      'name' => 'required|string|max:100',
+      'description' => 'required|string|max:100'
+
+    ]);
+
+
+    //2. cek validator error
+    if ($validator->fails()) {
+      return response()->json([
+        'success' => false,
+        'message' => $validator->errors()
+      ], 422);
+    }
+
+
+    //3. upload image
+
+
+
+    //4. insert data
+    $genre = Genre::create([
+      'name' => $request->name,
+      'description' => $request->description,
+    ]);
+
+
+    //5. response
+    return response()->json([
+      'success' => true,
+      'message' => 'Resource added successfully',
+      'data' => $genre
+    ], 201);
+  }
 }
